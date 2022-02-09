ff197af77876c55b4ec9a1dea5f6fe87b1798c34
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-02-08 22:38:27 -0500

    Lab without challenge

--- src/main/java/org/ada/school/service/impl/UserServiceMongoDB.java
+++ src/main/java/org/ada/school/service/impl/UserServiceMongoDB.java
@@ -19,44 +19,42 @@ import java.util.List;
 public class UserServiceMongoDB implements UserService {
     private final UserRepository userRepository;
 
-    public UserServiceMongoDB(@Autowired UserRepository userRepository )
-    {
+    public UserServiceMongoDB(@Autowired UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
-    public User create( User user ) {
-        System.out.println("*****************************************************************************");
-        System.out.println(user.getId());
-        System.out.println(user.getName());
-        System.out.println(user.getEmail());
-        System.out.println("*****************************************************************************");
-
+    public User create(User user) {
         userRepository.save(user);
         return user;
     }
 
     @Override
-    public User findById( String id )
-    {
+    public User findById(String id) {
         return userRepository.findById(id).get();
     }
 
     @Override
-    public List<User> all()
-    {
+    public List<User> all() {
         return userRepository.findAll();
     }
 
     @Override
-    public boolean deleteById( String id )
-    {
-        return false;
+    public boolean deleteById(String id) {
+        boolean deleted = true;
+        try {
+            if (findById(id) == null) throw new Exception();
+            userRepository.deleteById(id);
+        } catch (Exception exception) {
+            deleted = false;
+        }
+        return deleted;
     }
 
     @Override
-    public User update(UserDto userDto, String id )
-    {
-        return null;
+    public User update(UserDto userDto, String id) {
+        User user = findById(id);
+        user.update(userDto);
+        return userRepository.save(user);
     }
 }
